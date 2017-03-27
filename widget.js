68e5748efdd05dabfe6084859c7daff2bfef946b
Jess Eldredge <jess@eldredge.io>
2017-03-27 15:10:04 -0400

    Hook up form to search github repos

--- widget.js
+++ widget.js
@@ -1,8 +1,22 @@
-new Ajax('https://api.github.com/search/repositories?q={query}')
-  .then( x => console.log(x.items[0]))
-  .catch( e => {
-    console.error(e);
-    return 'There was an error with your request!';
-  })
-  .catch( x => alert(x))
-  .get();
+const form = document.getElementById('search-form');
+const input = document.getElementById('search-form-input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let val = input.value;
+
+  new Ajax(`https://api.github.com/search/repositories?q={${val}}`)
+    .then( x => {
+      var repos = x.items;
+
+      repos.forEach(function(repo){
+        console.log(repo.name);
+      })
+    })
+    .catch( e => {
+      console.error(e);
+      return 'There was an error with your request!';
+    })
+    .catch( x => alert(x))
+    .get();
+})
