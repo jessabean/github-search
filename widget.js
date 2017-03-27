c780056027cfea5622b997be5072b911bd8754f8
Jess Eldredge <jess@eldredge.io>
2017-03-27 16:03:20 -0400

    Set up constructors for list and search

--- widget.js
+++ widget.js
@@ -1,22 +1,52 @@
 const form = document.getElementById('search-form');
 const input = document.getElementById('search-form-input');
+const results = document.getElementById('search-results');
+
+class List {
+  constructor() {
+    this._list = document.createElement('ul');
+  }
+
+  createItem() {
+    return document.createElement('li');
+  }
+
+  addItem(item) {
+    this._list.appendChild(item);
+  }
+
+  renderList() {
+    return this._list;
+  }
+}
+
+class Search {
+  constructor() {
+    this._input = document.getElementById('search-form-input');
+    this._query = this._input.value;
+  }
+}
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  let val = input.value;
+  results.innerHTML = '';
+  let search = new Search();
 
-  new Ajax(`https://api.github.com/search/repositories?q={${val}}`)
+  new Ajax(`https://api.github.com/search/repositories?access_token=0769f07df1f34bacf719bd46c1ef19722c4c0d92&q={${search._query}}`)
     .then( x => {
-      var repos = x.items;
+      let repos = x.items;
+      let list = new List();
+      results.appendChild(list.renderList());
 
       repos.forEach(function(repo){
-        console.log(repo.name);
+        let item = list.createItem();
+        item.innerHTML = repo.name;
+        list.addItem(item);
       })
     })
     .catch( e => {
       console.error(e);
       return 'There was an error with your request!';
     })
-    .catch( x => alert(x))
     .get();
 })
